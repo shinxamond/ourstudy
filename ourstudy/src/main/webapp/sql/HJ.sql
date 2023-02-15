27c452bcdba19b3bc9dee2b7ff9349e85a05331a
hjjin <hjjin@DESKTOP-8FP6VFD>
2023-02-15 14:41:02 +0900

    진혜정 - 테이블 수정

--- ourstudy/src/main/webapp/sql/HJ.sql
+++ ourstudy/src/main/webapp/sql/HJ.sql
@@ -1,30 +1,17 @@
---독서실 이용권 테이블
-CREATE TABLE study(
- study_num number,
- study_name varchar2(30) not null,
- study_type number(1) not null,
- study_price number(8) not null,
+--이용권 테이블
+CREATE TABLE Ticket(
+ ticket_num number,
+ ticket_kind number(1) not null,
+ ticket_name varchar(30) not null,
+ ticket_type number(1) not null,
+ ticket_price number(8) not null,
  mem_num number not null,
- constraint study_pk primary key (study_num),
- constraint study_fk foreign key (mem_num) references member (mem_num)
+ constraint ticket_pk primary key (ticket_num),
+ constraint ticket_fk1 foreign key (mem_num) references  member (mem_num)
 );
 
--- 독서실 이용권 시퀀스
-create sequence study_seq;
-
--- 사물함 이용권 테이블
-CREATE TABLE locker(
- lock_num number,
- lock_name varchar2(30) not null,
- lock_type number(1) not null,
- lock_price number(8) not null,
- mem_num number not null,
- constraint locker_pk primary key (lock_num),
- constraint locker_fk foreign key (mem_num) references member (mem_num)
-);
-
--- 사물함 이용권 시퀀스
-create sequence locker_seq;
+-- 이용권 시퀀스
+create sequence ticket_seq;
 
 -- 결제 테이블
 CREATE TABLE pay(
@@ -41,3 +28,6 @@ CREATE TABLE pay(
  constraint pay_fk2 foreign key (study_num) references study (study_num),
  constraint pay_fk3 foreign key (lock_num) references locker (lock_num)
 );
+
+-- 결제 시퀀스
+create sequence pay_seq;
