b60b5c85416031d8651f69c99347e6aaeda9056d
kkonii <119216472+kkonii@users.noreply.github.com>
2023-02-22 16:40:15 +0900

    원경연 - seat_detail 테이블 수정 완료

--- ourstudy/src/main/webapp/sql/KY.sql
+++ ourstudy/src/main/webapp/sql/KY.sql
@@ -6,19 +6,22 @@ CREATE TABLE SEAT(
 );
 
 --좌석번호 
-CREATE SEQUENCE   SEAT_SEQ;
+CREATE SEQUENCE SEAT_SEQ;
 
 --좌석상세
 CREATE TABLE SEAT_DETAIL(
+	seat_detail_num number not null,
     seat_num number not null,
     mem_num number not null,
     mem_name varchar2(30) not null,
     in_time date default SYSDATE not null,
     out_time date,
     total_time number,
-    constraint seat_detail_pk primary key (seat_num),
+    constraint seat_detail_pk primary key (num),
     constraint seat_detail_fk1 foreign key (seat_num) references seat(seat_num),
-     constraint seat_detail_fk2 foreign key (mem_num) references member(mem_num)
+    constraint seat_detail_fk2 foreign key (mem_num) references member(mem_num)
 );
 
+--좌석상세
+CREATE SEQUENCE SEAT_DETAIL_SEQ;
 
