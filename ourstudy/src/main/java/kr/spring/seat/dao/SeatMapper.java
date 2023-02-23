faec4394914980be59bb8e91d34f62d455067fdf
kkonii <119216472+kkonii@users.noreply.github.com>
2023-02-23 16:09:27 +0900

    원경연 - 좌석 VO, Mapper 수정

--- ourstudy/src/main/java/kr/spring/seat/dao/SeatMapper.java
+++ ourstudy/src/main/java/kr/spring/seat/dao/SeatMapper.java
@@ -1,9 +1,7 @@
 package kr.spring.seat.dao;
 
-import java.sql.Date;
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -14,7 +12,7 @@ import kr.spring.seat.vo.SeatVO;
 @Mapper
 public interface SeatMapper {
 	/*==================================
-		    		관 리 자
+		    		ADMIN
 	  ==================================*/
 	//좌석번호
 	@Select("SELECT seat_seq.nextval FROM dual")
@@ -25,39 +23,59 @@ public interface SeatMapper {
 	
 	
 	/*==================================
-			     회       원
+			     	MEMBER
 	==================================*/
 	//좌석 선택 
+	@Select("SELECT seat_detail_seq.nextval FROM DUAL")
+	public int selectSeat_Detail_num();
 	//회원 정보, 입실 날짜 입력
-	@Insert("INSERT INTO SEAT_DETAIL(mem_num,mem_name,seat_num) VALUES (#{mem_num},#{mem_name},#{seat_num})")
-	public void selectSeat(SeatVO vo);
+	@Insert("INSERT INTO SEAT_DETAIL(seat_detail_num,mem_num,mem_name,seat_num) VALUES (seat_detail_seq.nextval,#{mem_num},#{mem_name},#{seat_num})")
+	public void insertToSelectSeat(SeatVO vo);
 	//좌석 이동(퇴실 처리후 입실)
 	//selectSeat -> deleteSeatDetail
 	
 	
-	/*========================================*/
+	/*========================================
+	 				LOGIC
+	 ======================================= */
 	//해당 DB에서 정보 가져오기
 	public List<SeatVO> getSeatList();
 	public List<SeatVO> getSeatDetailList();
-	//새좌석에 정보넣기
-	@Insert("INSERT INTO SEAT_DETAIL(mem_num,mem_name,in_time) VALUES(#{mem_num},#{mem_name},#{in_time}) WHERE seat_num=#{seat_num}")
-	public void insertSeatDetail(int seat_num);
-	//좌석 정보 모두 삭제(아예퇴실)
-	@Delete("DELETE SEAT_DETAIL WHERE seat_num=#{seat_num}")
-	public void deleteSeatDetail(int seat_num);
-	
 	//좌석이용시간 구하기
+	
+	//해당 멤버의 좌석 번호 구하기
+	@Select("SELECT seat_num FROM SEAT_DETAIL WHERE mem_num=#{mem_num} AND out_time IS NULL")
+	public int getSeat_numByMem_num(int mem_num);
 	//좌석 상태 변경
 	//외출상태로 변경
-	@Update("UPDATE SEAT SET seat_status=2 WHERE seat_num=#{seat_num}")
-	public void SeatStatusHold(int seat_num);
+	@Update("UPDATE SEAT SET seat_status=2 WHERE seat_num=#{seat_num}") 
+	public void seatStatusHold(int seat_num);
 	//사용불가상태로 변경
-	@Delete("DELETE SEAT SET seat_status=0 WHERE seat_num=#{seat_num}")
+	@Update("UPDATE SEAT SET seat_status=0 WHERE seat_num=#{seat_num}")
 	public void seatStatusIn(SeatVO vo);
 	//사용가능 상태로 변경
+	@Update("UPDATE SEAT SET seat_status=1 WHERE seat_num=#{seat_num}")
 	public void seatStatusOut(int seat_num);
 	//퇴실날짜정보 등록
-	@Update("UPDATE SEAT_DETAIL SET out_time=SYSDATE WHERE seat_num=#{seat_num}")
-	public void outSeat(int seat_num);
+	@Update("UPDATE SEAT_DETAIL SET out_time=SYSDATE WHERE seat_num=#{seat_num} AND out_time IS NULL")
+	public void insertOutTimeBySeat_num(int seat_num);
+	@Update("UPDATE SEAT_DETAIL SET out_time=SYSDATE WHERE mem_num=#{mem_num} AND out_time IS NULL")
+	public void insertOutTimeByMem_num(int mem_num);
+	
+	//total_time 이 null인 회원의 입실날짜와 퇴실날짜를 String으로 불러온다.
+	@Select("SELECT TO_CHAR(in_time, 'yyyy-MM-dd HH24:MI:SS') in_time, TO_CHAR(out_time, 'yyyy-MM-dd HH24:MI:SS') out_time FROM SEAT_DETAIL WHERE seat_num=#{seat_num} AND total_time IS NULL")
+	public SeatVO getTimes(int seat_num);
+	//퇴실날짜 저장
+	@Update("UPDATE SEAT_DETAIL SET total_time=#{total_time} WHERE seat_num=#{seat_num} AND total_time IS NULL")
+	public void insertTotal_time(SeatVO vo);
+	/*===========================================
+	 				KEEP
+	  ===========================================*/
+	//현재 좌석의 컬럼수
+	@Select("SELECT COUNT(*) FROM SEAT_DETAIL WHERE seat_num=#{seat_num}")
+	public int getSeatCount(SeatVO vo);
+	//좌석 이동시 새로 선택한 좌석에 정보넣기
+	@Insert("INSERT INTO SEAT_DETAIL(mem_num,mem_name,in_time) VALUES(#{mem_num},#{mem_name},#{in_time}) WHERE seat_num=#{seat_num}")
+	public void insertToMoveSeat(int seat_num);
 	
 }
--- ourstudy/src/main/java/kr/spring/seat/vo/SeatVO.java
+++ ourstudy/src/main/java/kr/spring/seat/vo/SeatVO.java
@@ -1,10 +1,9 @@
 package kr.spring.seat.vo;
 
-import java.sql.Date;
-
 import javax.validation.constraints.NotEmpty;
 
 public class SeatVO {
+	private int seat_detail_num;
 	private int seat_num;
 	private int seat_status;
 	@NotEmpty
@@ -12,9 +11,16 @@ public class SeatVO {
 	@NotEmpty
 	private int mem_num;
 	private String mem_name;
-	private Date in_time;
-	private Date out_time;
-	private long total_time;
+	private String in_time;
+	private String out_time;
+	private int total_time;
+	
+	public int getSeat_detail_num() {
+		return seat_detail_num;
+	}
+	public void setSeat_detail_num(int seat_detail_num) {
+		this.seat_detail_num = seat_detail_num;
+	}
 	
 	public int getSeat_num() {
 		return seat_num;
@@ -46,30 +52,29 @@ public class SeatVO {
 	public void setMem_name(String mem_name) {
 		this.mem_name = mem_name;
 	}
-	public Date getIn_time() {
+	public String getIn_time() {
 		return in_time;
 	}
-	public void setIn_time(Date in_time) {
+	public void setIn_time(String in_time) {
 		this.in_time = in_time;
 	}
-	public Date getOut_time() {
+	public String getOut_time() {
 		return out_time;
 	}
-	public void setOut_time(Date out_time) {
+	public void setOut_time(String out_time) {
 		this.out_time = out_time;
 	}
-	public long getTotal_time() {
+	public int getTotal_time() {
 		return total_time;
 	}
-	public void setTotal_time(long total_time) {
+	public void setTotal_time(int total_time) {
 		this.total_time = total_time;
 	}
 	@Override
 	public String toString() {
-		return "SeatVO [seat_num=" + seat_num + ", seat_status=" + seat_status + ", seat_name=" + seat_name
-				+ ", mem_num=" + mem_num + ", mem_name=" + mem_name + ", in_time=" + in_time + ", out_time=" + out_time
-				+ ", total_time=" + total_time + "]";
+		return "SeatVO [seat_detail_num=" + seat_detail_num + ", seat_num=" + seat_num + ", seat_status=" + seat_status
+				+ ", seat_name=" + seat_name + ", mem_num=" + mem_num + ", mem_name=" + mem_name + ", in_time="
+				+ in_time + ", out_time=" + out_time + ", total_time=" + total_time + "]";
 	}
-	
 	
 }
