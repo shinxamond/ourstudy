ede8e81867f9010c2f211293caaf7251ceaa98e1
kkonii <119216472+kkonii@users.noreply.github.com>
2023-02-23 19:59:25 +0900

    원경연 - LOCKER_DETAIL 테이블에 locker_detail_num 필드 추가

--- ourstudy/src/main/webapp/sql/JH.sql
+++ ourstudy/src/main/webapp/sql/JH.sql
@@ -10,14 +10,18 @@ CREATE SEQUENCE locker_seq;
 
 -- locker_detail Table Create SQL
 CREATE TABLE locker_detail
-( 
+( 	
+	locker_detail_num NUMBER NOT NULL,
     locker_num NUMBER NOT NULL,
     mem_num NUMBER NOT NULL,
     mem_name VARCHAR2(30) NOT NULL,
     locker_start DATE default SYSDATE NOT NULL,
     locker_end DATE,
+    locker_diff NUMBER,
     
-    CONSTRAINT locker_detail_pk PRIMARY KEY (locker_num),
+    CONSTRAINT locker_detail_pk PRIMARY KEY (locker_detail_num),
     CONSTRAINT locker_detail_fk FOREIGN KEY (locker_num) REFERENCES locker_info(locker_num),
     CONSTRAINT locker_detail_fk2 FOREIGN KEY (mem_num) REFERENCES member(mem_num)
 );
+
+CREATE SEQUENCE locker_detail_seq;
