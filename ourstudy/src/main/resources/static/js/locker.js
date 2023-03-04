99ee3aacfa8d45c8f69bc658fd6a271c875ac536
kkonii <119216472+kkonii@users.noreply.github.com>
2023-03-05 03:29:44 +0900

    원경연 - 사물함 등록 JS 생성

---
+++ ourstudy/src/main/resources/static/js/locker.js
@@ -0,0 +1,18 @@
+
+/*==============================
+		사물함 선택 확인 alert
+  =============================*/
+    function checkSelect(e) {
+    	locker_num = document.getElementById(e.getAttribute('id')).getAttribute('id');
+    	
+    	let select = confirm(locker_num +'번 사물함을 선택할까요?');
+    	
+    	if(select == false) {
+    		alert('사물함 선택이 취소되었습니다.');
+    		return false;
+    	}else if(select == true){
+    		alert(locker_num + '번 사물함이 선택되었습니다.');
+    		
+    		location.href='/locker/select.do?locker_num=' + locker_num;
+    	}
+    }
