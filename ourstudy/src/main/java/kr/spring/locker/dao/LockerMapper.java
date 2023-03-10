c85c3fc9dd859701214d670f6c41435d5da1c12f
kkonii <119216472+kkonii@users.noreply.github.com>
2023-03-10 14:56:11 +0900

    Locker up

--- ourstudy/src/main/java/kr/spring/locker/dao/LockerMapper.java
+++ ourstudy/src/main/java/kr/spring/locker/dao/LockerMapper.java
@@ -8,52 +8,53 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import kr.spring.locker.vo.LockerVO;
+import kr.spring.pay.vo.PayVO;
 
 @Mapper
 public interface LockerMapper {
-	/*==================================
-				ADMIN
-	====================================*/
-	//사물함 번호
-	@Select("SELECT locker_info_seq.nextval FROM dual")
-	public int selectLocker_num();
-	//좌석 등록
-	@Insert("INSERT INTO LOCKER_INFO(locker_num) VALUES(#{locker_num})")
-	public void insertLocker(LockerVO vo);
+   /*==================================
+            ADMIN
+   ====================================*/
+   //사물함 번호
+   @Select("SELECT locker_info_seq.nextval FROM dual")
+   public int selectLocker_num();
+   //좌석 등록
+   @Insert("INSERT INTO LOCKER_INFO(locker_num) VALUES(#{locker_num})")
+   public void insertLocker(LockerVO vo);
 
-	/*==================================
-     			MEMBER
-	==================================*/
-	//사물함 선택
-	//사물함 회원정보,이용시작시간 자동입력 (사물함을 선택했을 때)
-	@Insert("INSERT INTO LOCKER_DETAIL (locker_detail_num,locker_num,mem_num,mem_name) VALUES(locker_detail_seq.nextval,#{locker_num},#{mem_num},#{mem_name})")
-	public void insertToSelectLocker(LockerVO vo);
-	
-	/*==================================
-     			IN / OUT / DATE
-	==================================*/
-	//이용시작 시간 + 1주/2주/4주
-	//이용시작 시간 추출
-	@Select("SELECT TO_CHAR(locker_start, 'yyyy-MM-dd HH24:MI:SS') locker_start FROM LOCKER_DETAIL WHERE mem_num=#{mem_num} AND locker_diff IS NULL")
-	public String getLocker_start(LockerVO vo);
-	//종료시간, 남은시간 입력
-	@Update("UPDATE LOCKER_DETAIL SET locker_end=TO_DATE(#{locker_end},'yy/MM/dd hh24:mi:ss'),locker_diff=#{locker_diff} WHERE mem_num=#{mem_num} AND locker_diff IS NULL")
-	public void insertEndAndDiff(LockerVO vo);
-	//사용불가상태
-	@Update("UPDATE LOCKER_INFO SET locker_status=0 WHERE locker_num=#{locker_num}")
-	public void lockerStatusIn(LockerVO vo);
-	//사용가능상태
-	@Update("UPDATE LOCKER_INFO SET locker_status=1 WHERE locker_num=#{locker_num}")
-	public void lockerStatusOut(LockerVO vo);
-	
-	/*==================================
-     			GET DATA
-	==================================*/
-	//사물함 정보 불러오기
-	@Select("SELECT * FROM LOCKER_INFO ORDER BY LOCKER_NUM ASC")
-	public List<LockerVO> getLockerList();
-	
-	//기존에 등록된 사물함 check 갯수 불러오기
-	@Select("SELECT count(*) FROM LOCKER_INFO i JOIN (SELECT * FROM LOCKER_DETAIL WHERE mem_num=#{mem_num}) d ON d.locker_num=i.locker_num WHERE i.locker_status=0")
-	public int getMyLockerCount(int mem_num);
+   /*==================================
+              MEMBER
+   ==================================*/
+   //사물함 선택
+   //사물함 회원정보,이용시작시간 자동입력 (사물함을 선택했을 때)
+   @Insert("INSERT INTO LOCKER_DETAIL (locker_detail_num,locker_num,mem_num,mem_name) VALUES(locker_detail_seq.nextval,#{locker_num},#{mem_num},#{mem_name})")
+   public void insertToSelectLocker(LockerVO vo);
+   
+   /*==================================
+              IN / OUT / DATE
+   ==================================*/
+   //이용시작 시간 + 1주/2주/4주
+   //이용시작 시간 추출
+   @Select("SELECT TO_CHAR(locker_start, 'yyyy-MM-dd HH24:MI:SS') locker_start FROM LOCKER_DETAIL WHERE mem_num=#{mem_num} AND locker_diff IS NULL")
+   public String getLocker_start(LockerVO vo);
+   //종료시간, 남은시간 입력
+   @Update("UPDATE LOCKER_DETAIL SET locker_end=TO_DATE(#{locker_end},'yy/MM/dd hh24:mi:ss'),locker_diff=#{locker_diff} WHERE mem_num=#{mem_num} AND locker_diff IS NULL")
+   public void insertEndAndDiff(LockerVO vo);
+   //사용불가상태
+   @Update("UPDATE LOCKER_INFO SET locker_status=0 WHERE locker_num=#{locker_num}")
+   public void lockerStatusIn(PayVO vo);
+   //사용가능상태
+   @Update("UPDATE LOCKER_INFO SET locker_status=1 WHERE locker_num=#{locker_num}")
+   public void lockerStatusOut(LockerVO vo);
+   
+   /*==================================
+              GET DATA
+   ==================================*/
+   //사물함 정보 불러오기
+   @Select("SELECT * FROM LOCKER_INFO ORDER BY LOCKER_NUM ASC")
+   public List<LockerVO> getLockerList();
+   
+   //기존에 등록된 사물함 check 갯수 불러오기
+   @Select("SELECT count(*) FROM LOCKER_INFO i JOIN (SELECT * FROM LOCKER_DETAIL WHERE mem_num=#{mem_num}) d ON d.locker_num=i.locker_num WHERE i.locker_status=0")
+   public int getMyLockerCount(int mem_num);
 }
--- ourstudy/src/main/java/kr/spring/locker/service/LockerServiceImpl.java
+++ ourstudy/src/main/java/kr/spring/locker/service/LockerServiceImpl.java
@@ -8,78 +8,79 @@ import org.springframework.transaction.annotation.Transactional;
 
 import kr.spring.locker.dao.LockerMapper;
 import kr.spring.locker.vo.LockerVO;
+import kr.spring.pay.vo.PayVO;
 
 @Service
 @Transactional
 public class LockerServiceImpl implements LockerService{
 
-	@Autowired
-	private LockerMapper lockerMapper;
-	
-	/*=================================
-					ADMIN
-	  =================================*/
-	//새로운 사물함 등록
-	@Override
-	public void insertLocker(LockerVO vo) {
-		vo.setLocker_num(lockerMapper.selectLocker_num());
-		lockerMapper.insertLocker(vo);
-	}
+   @Autowired
+   private LockerMapper lockerMapper;
+   
+   /*=================================
+               ADMIN
+     =================================*/
+   //새로운 사물함 등록
+   @Override
+   public void insertLocker(LockerVO vo) {
+      vo.setLocker_num(lockerMapper.selectLocker_num());
+      lockerMapper.insertLocker(vo);
+   }
 
-	/*=================================
-					MEMBER
-	  =================================*/
-	//사물함 선택
-	@Override
-	public void selectLocker(LockerVO vo) {
-		lockerMapper.insertToSelectLocker(vo);
-		lockerMapper.lockerStatusIn(vo);
-	}
-	
-	/*==================================
-     			GET DATA
-	==================================*/
-	//사물함 정보 불러오기
-	@Override
-	public List<LockerVO> getLockerList() {
-		return lockerMapper.getLockerList();
-	}
+   /*=================================
+               MEMBER
+     =================================*/
+   //사물함 선택
+   @Override
+   public void selectLocker(LockerVO vo) {
+      lockerMapper.insertToSelectLocker(vo);
+//      lockerMapper.lockerStatusIn(vo);
+   }
+   
+   /*==================================
+              GET DATA
+   ==================================*/
+   //사물함 정보 불러오기
+   @Override
+   public List<LockerVO> getLockerList() {
+      return lockerMapper.getLockerList();
+   }
 
-	@Override
-	public int getMyLockerCount(int mem_num) {
-		return lockerMapper.getMyLockerCount(mem_num);
-	}
-	
-	/*=================================
-				IN / OUT / DATE
-	  =================================*/
-	//시작일 불러오기
-	@Override
-	public String getLocker_start(LockerVO vo) {
-		return lockerMapper.getLocker_start(vo);
-	}
+   @Override
+   public int getMyLockerCount(int mem_num) {
+      return lockerMapper.getMyLockerCount(mem_num);
+   }
+   
+   /*=================================
+            IN / OUT / DATE
+     =================================*/
+   //시작일 불러오기
+   @Override
+   public String getLocker_start(LockerVO vo) {
+      return lockerMapper.getLocker_start(vo);
+   }
 
-	//종료일, 남은시각 저장 (퇴실 시)
-	@Override
-	public void insertEndAndDiff(LockerVO vo) {
-		lockerMapper.insertEndAndDiff(vo);
-	}
+   //종료일, 남은시각 저장 (퇴실 시)
+   @Override
+   public void insertEndAndDiff(LockerVO vo) {
+      lockerMapper.insertEndAndDiff(vo);
+   }
 
-	//사물함 이용불가 상태로 변경
-	@Override
-	public void lockerStatusIn(LockerVO vo) {
-		lockerMapper.lockerStatusIn(vo);
-	}
+   //사물함 이용불가 상태로 변경
+   @Override
+   public void lockerStatusIn(PayVO vo) {
+      lockerMapper.lockerStatusIn(vo);
+   }
 
-	//사물함 이용가능 상태로 변경
-	@Override
-	public void lockerStatusOut(LockerVO vo) {
-		lockerMapper.lockerStatusOut(vo);
-	}
+   //사물함 이용가능 상태로 변경
+   @Override
+   public void lockerStatusOut(LockerVO vo) {
+      lockerMapper.lockerStatusOut(vo);
+   }
 
 
-	
-	
+   
+   
 
 
 }
