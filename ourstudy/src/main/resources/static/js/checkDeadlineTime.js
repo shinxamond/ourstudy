fdd9726943c082c5bbac3c9a2ea56d9c7c935281
NOTE <NOTE@DESKTOP-3EL4SRV>
2023-03-13 13:46:34 +0900

    최영민 - 잔여시간 계산 10초 -> 60초

--- ourstudy/src/main/resources/static/js/checkDeadlineTime.js
+++ ourstudy/src/main/resources/static/js/checkDeadlineTime.js
@@ -50,7 +50,7 @@ $(function(){
 	  		
 			console.log(updateSec);
 	
-	  		if(updateSec >= 10) {
+	  		if(updateSec >= 60) {
 	  			sessionStorage.setItem('plusSec', '0');
 	  			
 	  			let mem_num = sessionStorage.getItem('isSelectMemnum');
